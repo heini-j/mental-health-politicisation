e70aaf2792c7dac01dc34f6d0d88d25472e5d1f3
Heini Järviö <heinimjarv@gmail.com>
2025-10-03 16:06:07 +0200

    added filters

--- API.R
+++ API.R
@@ -11,7 +11,15 @@ mp_maindataset()
 
 my_corpus_df <- mp_corpus(countryname == "Austria" & edate > as.Date("2000-01-01"), translation = "en",
                           as_tibble = TRUE)
+
+# adding row numbers to help identifying documents later
+corpus_id <- my_corpus_df |>
+  mutate(rownumber = row_number())
+
+# creating a list of relevant keywords
 keywords <- c("mental health", "mental illness", "depression", "anxiety", "psych", "therapy")
-corpus_filtered <- my_corpus_df |>
+
+# filtering with keywords
+corpus_filtered <- corpus_id |>
   filter(grepl(paste0("\\b",paste(keywords, collapse="\\b|\\b"),"\\b"), text))
 
