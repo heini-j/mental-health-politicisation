e87779892ba20553d9066aa11c66d0a23e0dff43
Heini Järviö <heinimjarv@gmail.com>
2025-10-17 17:06:28 +0200

    attempts

--- cleaning.R
+++ cleaning.R
@@ -1,5 +1,6 @@
 library(readr)
 library(dplyr)
+library(fuzzyjoin)
 
 # Loading the manifesto file to R ----
 
@@ -16,11 +17,32 @@ ches_trend_1999_2019 <- read_csv("1999-2019_CHES_dataset_means(v3).csv", na = ""
   select(countrycode = country, party_id, party, year)
 
 partyfacts <- 
-  read_csv("partyfacts-core-parties.csv", na = "", locale = locale(encoding = "UTF-8")) |> 
-  filter(dataset_key == "ches") |> 
-  select(dataset_party_id, partyfacts_id) |> 
+  read_csv("partyfacts-external-parties.csv", na = "", locale = locale(encoding = "UTF-8")) |> 
+  filter(dataset_key %in% c("manifesto")) |> 
+  #select(dataset_party_id, partyfacts_id) |> 
   mutate(dataset_party_id = as.numeric(dataset_party_id)) |> 
   distinct()
+
+ches_europe <- 
+  bind_rows(
+    ches_trend_1999_2019 |> left_join(ches_countries, by = c("countrycode")) |> select(countryshort, year, party_id, party))
+
+ches_all <- 
+  bind_rows(
+    ches_europe |> mutate(party_id = as.numeric(party_id)))
+
+ches <- 
+  ches_all |> 
+  mutate(
+    year_first = min(year, na.rm = TRUE),
+    year_last = max(year, na.rm = TRUE),
+    .by = c(party_id)
+  ) |> 
+  select(-year) |> 
+  distinct() |> 
+  slice(1L, .by = c("party_id"))
+
+ches$party_id |> duplicated() |> any()
 
 ches_partyfacts <- 
   ches |> 
@@ -39,7 +61,19 @@ length(repeated)
 # Let's remove the duplicates keeping only first occurence of each unique value
 
 df_clean <- data_australia |>
-  distinct(text, .keep_all = TRUE)
+  distinct(text, .keep_all = TRUE) |>
+  rename("dataset_party_id" = "party")
 
 View(df_clean)
 
+df_joined <- fuzzy_left_join(
+  df_clean, partyfacts,
+  by = c(
+    "party_id" = "dataset_party_id",
+    "year_first" ="year",
+    "year_last" ="year"
+  ),
+  match_fun = list(`==`, `>=`, `<=`)
+)
+
+?fuzzy_inner_join
