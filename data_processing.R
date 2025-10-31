5152028b699e6470b3642c77eaad2b83a099168f
Heini Järviö <heinimjarv@gmail.com>
2025-10-31 14:04:40 +0100

    descriptives

--- data_processing.R
+++ data_processing.R
@@ -1,23 +1,54 @@
-# Viewing unclear rows to decide whether to keep or delete them
+library(readr)
+library(dplyr)
+library(ggplot2)
 
-View(corpus_id |>
-       slice(42010:42015))
+# Reading data to R ------
 
+data_australia <- read_csv("data/corpus_Australia.csv")
 
-# creating a new df that counts the number of rows per year
+data_australia <- data_australia |>
+  mutate(party = as.character(party))
 
-corpus_count <- corpus_filtered |>
+# Getting basic counts -----
+
+# How many rows per year?
+
+corpus_count <- data_australia |>
   group_by(year) |>
   summarise(count = n()) |>
   arrange(year)
-# viewing the counts per year
 
 View(corpus_count)
 
-# checking count of rows per party
+# How many rows per party?
 
-corpus_party_count <- corpus_filtered |>
+corpus_party_count <- data_australia |>
   group_by(party) |>
   summarise(count = n()) |>
   arrange(desc(count))
+
 View(corpus_party_count)
+
+# How many rows per party per year?
+
+corpus_party_year_count <- data_australia |>
+  group_by(party, year) |>
+  summarise(count = n()) |>
+  arrange(party, year)
+
+View(corpus_party_year_count)
+
+# Visualizing counts over time -----
+
+# Creating a line plot showing the count of rows per year for each party
+
+ggplot(corpus_party_year_count, aes(x = year, y = count, color = party, group = party)) +
+  geom_line() +
+  labs(title = "Count of Manifesto Rows per Year",
+       x = "Year",
+       y = "Count of Rows") +
+  theme_minimal() + 
+  scale_color_brewer(palette = "Set1")
+
+
+?geom_line
