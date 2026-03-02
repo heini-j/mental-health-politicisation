b065d676e296fd66ce01523b33994dc1d22da76b
Heini Järviö <heinimjarv@gmail.com>
2026-03-02 15:40:57 +0100

    basic descriptives

--- data_processing.R
+++ data_processing.R
@@ -2,53 +2,42 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
+# This script is for descriptive analysis of the corpus data 
+
 # Reading data to R ------
 
-data_australia <- read_csv("data/corpus_Australia.csv")
+data_belgium <- read_csv("data/belgium_with_ches.csv")
 
-data_australia <- data_australia |>
-  mutate(party = as.character(party))
+# Basic descriptives -----
 
-# Getting basic counts -----
+# Adding together all values in the "count" column to get the total count for Belgium
+
+total_count_belgium <- sum(data_belgium$count) # 502 in total
 
 # How many rows per year?
 
-corpus_count <- data_australia |>
+corpus_count <- data_belgium |>
   group_by(year) |>
-  summarise(count = n()) |>
+  summarise(count_yearly = sum(count)) |>
   arrange(year)
 
 View(corpus_count)
 
+unique(data_belgium$year)
+
 # How many rows per party?
 
-corpus_party_count <- data_australia |>
-  group_by(party) |>
-  summarise(count = n()) |>
-  arrange(desc(count))
+corpus_party_count <- data_belgium |>
+  group_by(name_bilingual) |>
+  summarise(count_party = sum(count)) 
 
 View(corpus_party_count)
 
 # How many rows per party per year?
 
-corpus_party_year_count <- data_australia |>
-  group_by(party, year) |>
-  summarise(count = n()) |>
-  arrange(party, year)
+corpus_party_year_count <- data_belgium |>
+  group_by(name_bilingual, year) |>
+  summarise(count_party_year = sum(count))
 
 View(corpus_party_year_count)
 
-# Visualizing counts over time -----
-
-# Creating a line plot showing the count of rows per year for each party
-
-ggplot(corpus_party_year_count, aes(x = year, y = count, color = party, group = party)) +
-  geom_line() +
-  labs(title = "Count of Manifesto Rows per Year",
-       x = "Year",
-       y = "Count of Rows") +
-  theme_minimal() + 
-  scale_color_brewer(palette = "Set1")
-
-
-?geom_line
